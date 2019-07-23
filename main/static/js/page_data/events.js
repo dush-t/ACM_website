3c8e13864aa7fca34cfe29d519474a1a212eca2f
samina-mulani <43271817+samina-mulani@users.noreply.github.com>
2019-07-23 23:01:24 +0530

    Added more elements
    Added more elements for contact section and navbar active list item

--- main/static/js/page_data/events.js
+++ main/static/js/page_data/events.js
@@ -16,17 +16,27 @@ const list_item = {
     "insert_position": "beforeend"
 };
 
-const landing_screen = {
+
+const list_item_active = {
     "position": 3,
+    "parent_elem_class": ".nav",
+    "parent_elem_type": "ul",
+    "index": false,
+    "template_name": "#navbar-list-item-active",
+    "insert_position": "beforeend"
+};
+
+const landing_screen = {
+    "position": 4,
     "parent_elem_class": ".event-landing-container",
     "parent_elem_type": "div",
     "index": false,
     "template_name": "#landing_screen",
-    "insert-position": "beforeend"
+    "insert_position": "beforeend"
 };
 
 const event_section_even_template = {
-    "position": 4,
+    "position": 5,
     "parent_elem_class": ".event-section-container",
     "parent_elem_type": "div",
     "index": false,
@@ -35,7 +45,7 @@ const event_section_even_template = {
 };
 
 const event_section_odd_template = {
-    "position": 5,
+    "position": 6,
     "parent_elem_class": ".event-section-container",
     "parent_elem_type": "div",
     "index": false,
@@ -44,16 +54,16 @@ const event_section_odd_template = {
 };
 
 const contact_section_about = {
-    "position": 6,
+    "position": 7,
     "parent_elem_class": ".contact-section",
     "parent_elem_type": "div",
     "index": false,
     "template_name": "#contact-section-about",
-    "insert_position": "beforeend"
+    "insert_position": "afterbegin"
 };
 
 const contact_section_links = {
-    "position": 7,
+    "position": 8,
     "parent_elem_class": ".contact_section_links_container",
     "parent_elem_type": "ul",
     "index": false,
@@ -61,23 +71,47 @@ const contact_section_links = {
     "insert_position": "beforeend"
 };
 
+const where_to_find_us = {
+    "position": 9,
+    "parent_elem_class": ".whereToFindUs",
+    "parent_elem_type": "div",
+    "index": false,
+    "template_name": "#where_to_find_us",
+    "insert_position": "beforeend"
+};
+
+const email_us = {
+    "position": 10,
+    "parent_elem_class": ".emailUs",
+    "parent_elem_type": "div",
+    "index": false,
+    "template_name": "#email_us",
+    "insert_position": "beforeend"
+};
+
 const pageStructure = [
     "events_page",
     [navbar_logo],
     [list_item],
+    [list_item_active],
     [landing_screen],
     [event_section_even_template],
     [event_section_odd_template],
     [contact_section_about],
-    [contact_section_links]
+    [contact_section_links],
+    [where_to_find_us],
+    [email_us]
 ];
 
 const positionNames = {
     1: "ACM Logo",
     2: "Navbar page links",
-    3: "Page landing screen",
-    4: "Event section odd position",
-    5: "Event section even position",
-    6: "About us",
-    7: "Follow us"
+    3: "Navbar page links active",
+    4: "Page landing screen",
+    5: "Event section odd position",
+    6: "Event section even position",
+    7: "About us",
+    8: "Follow us",
+    9: "Where to Find Us",
+    10: "Email Us",
 };
