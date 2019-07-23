16aa8d91373fb345870292bc35ede88cb3fe6401
samina-mulani <43271817+samina-mulani@users.noreply.github.com>
2019-07-23 23:02:46 +0530

    Added js for members

--- main/static/js/page_data/members.js
+++ main/static/js/page_data/members.js
@@ -16,13 +16,13 @@ const list_item = {
     "insert_position": "beforeend"
 };
 
-const landing_screen = {
+const list_item_active = {
     "position": 3,
-    "parent_elem_class": ".event-landing-container",
-    "parent_elem_type": "div",
+    "parent_elem_class": ".nav",
+    "parent_elem_type": "ul",
     "index": false,
-    "template_name": "#landing_screen",
-    "insert-position": "beforeend"
+    "template_name": "#navbar-list-item-active",
+    "insert_position": "beforeend"
 };
 
 const member_page_heading = {
@@ -31,7 +31,7 @@ const member_page_heading = {
     "parent_elem_type": "span",
     "index": false,
     "template_name": "#member-page-heading",
-    "insert-position": "beforeend"
+    "insert_position": "beforeend"
 };
 
 const member_page_subheading_1 = {
@@ -40,7 +40,7 @@ const member_page_subheading_1 = {
     "parent_elem_type": "span",
     "index": false,
     "template_name": "#member-page-subheading",
-    "insert-position": "beforeend"
+    "insert_position": "beforeend"
 };
 
 const postholder_member = {
@@ -49,7 +49,7 @@ const postholder_member = {
     "parent_elem_type": "div",
     "index": false,
     "template_name": "#postholder-member",
-    "insert-position": "beforeend"
+    "insert_position": "beforeend"
 };
 
 const member_page_subheading_2 = {
@@ -58,7 +58,7 @@ const member_page_subheading_2 = {
     "parent_elem_type": "span",
     "index": false,
     "template_name": "#member-page-subheading",
-    "insert-position": "beforeend"
+    "insert_position": "beforeend"
 };
 
 const core_member = {
@@ -67,7 +67,7 @@ const core_member = {
     "parent_elem_type": "div",
     "index": false,
     "template_name": "#core-member",
-    "insert-position": "beforeend"
+    "insert_position": "beforeend"
 };
 
 const contact_section_about = {
@@ -76,7 +76,7 @@ const contact_section_about = {
     "parent_elem_type": "div",
     "index": false,
     "template_name": "#contact-section-about",
-    "insert_position": "beforeend"
+    "insert_position": "afterbegin"
 };
 
 const contact_section_links = {
@@ -88,29 +88,55 @@ const contact_section_links = {
     "insert_position": "beforeend"
 };
 
+const where_to_find_us = {
+    "position": 11,
+    "parent_elem_class": ".whereToFindUs",
+    "parent_elem_type": "div",
+    "index": false,
+    "template_name": "#where_to_find_us",
+    "insert_position": "beforeend"
+};
+
+const email_us = {
+    "position": 12,
+    "parent_elem_class": ".emailUs",
+    "parent_elem_type": "div",
+    "index": false,
+    "template_name": "#email_us",
+    "insert_position": "beforeend"
+};
+
+
+
 const pageStructure = [
     "members_page",
     [navbar_logo],
     [list_item],
-    [landing_screen],
+    [list_item_active],
     [member_page_heading],
     [member_page_subheading_1],
     [postholder_member],
     [member_page_subheading_2],
     [core_member],
     [contact_section_about],
-    [contact_section_links]
+    [contact_section_links],
+    [where_to_find_us],
+    [email_us]
+
 ];
 
 const positionNames = {
     1: "ACM Logo",
     2: "Navbar page links",
-    3: "Page landing screen",
+    3: "Navbar page active links",
     4: "Member page heading",
     5: "Member page subheading 1",
     6: "Postholder members",
     7: "Member page subheading 2",
-    8: "Core members"
+    8: "Core members",
     9: "About us",
-    10: "Follow us"
+    10: "Follow us",
+    11: "Where to Find Us",
+    12: "Email Us",
+    
 };
